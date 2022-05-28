11143f7475b0807694693584f09e6abbaf63a1e1
Salma Hassan <salma.hassan.kamaleldin@gmail.com>
2022-05-28 16:22:37 +0200

    fix script

--- test-script.sh
+++ test-script.sh
@@ -2,7 +2,7 @@
 # currentBranch=`git branch --show-current`
 # echo $currentIntegrationBranch
 # echo $currentBranch
-`git diff --name-only master feature/test-branching | tr '\n' ',' | sed 's/\(.*\),/\1 /'`
+echo `git diff --name-only master feature/test-branching | tr '\n' ',' | sed 's/\(.*\),/\1 /'`
 #fileDiff=`git diff --name-only $currentBranch $currentIntegrationBranch`
 # filesInclusion="\n sonar.inclusions=$fileDiff"
 # cd ..
